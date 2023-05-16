2aaf87a9f033ef6b2b0ebd7d42a48f115b7cf134
Dawid <tetrin22@gmail.com>
2023-05-16 12:11:29 +0200

    Created function that adds new Task

--- js/script.js
+++ js/script.js
@@ -2,21 +2,35 @@ let todoInput; // użytkownik wpisuje tutaj treść
 let errorInfo; // info o braku zadań / konieczności wpisania tekstu
 let addBtn; // przycisk ADD - dodaje nowe elementy do listy
 let ulList; // lista zadań, tagi UL
+let newTask; // nowo dadany LI, (zadanie)
 
 const main = () => {
-    prepareDOMElements()
-    prepareDOMEvents()
-}
+	prepareDOMElements();
+	prepareDOMEvents();
+};
 
 const prepareDOMElements = () => {
-    todoInput = document.querySelector('.todo-input')
-    errorInfo = document.querySelector('.error-info')
-    addBtn = document.querySelector('.btn-add')
-    ulList = document.querySelector('.todolist ul')
-}
+	todoInput = document.querySelector(".todo-input");
+	errorInfo = document.querySelector(".error-info");
+	addBtn = document.querySelector(".btn-add");
+	ulList = document.querySelector(".todolist ul");
+};
 
 const prepareDOMEvents = () => {
-    
-}
+	addBtn.addEventListener("click", addNewTask);
+};
 
-document.addEventListener('DOMContentLoaded',main)
+const addNewTask = () => {
+	if (todoInput.value != "") {
+		newTask = document.createElement("li");
+		newTask.textContent = todoInput.value;
+		ulList.append(newTask);
+
+		todoInput.value = "";
+		errorInfo.textContent = "";
+	} else {
+		errorInfo.textContent = "Wpisz treść zadania!";
+	}
+};
+
+document.addEventListener("DOMContentLoaded", main);
