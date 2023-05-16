c0615419140ca95cb3d1043956218d663debfd24
Dawid <tetrin22@gmail.com>
2023-05-16 13:36:06 +0200

    Created functions for compliting task and editing task

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ let errorInfo; // info o braku zadań / konieczności wpisania tekstu
 let addBtn; // przycisk ADD - dodaje nowe elementy do listy
 let ulList; // lista zadań, tagi UL
 
+let popup; // popup
+let popupInfo; // tekst w popupie, jak się doda pusty tekst
+let todoToEdit; // edytowany Todo
+let popupInput; // input w popupie
+let popupAddBtn; // przycisk "zatwierdź" w popupie
+let popupCloseBtn; // przycisk "anuluj" w popupie
+
 const main = () => {
 	prepareDOMElements();
 	prepareDOMEvents();
@@ -13,10 +20,19 @@ const prepareDOMElements = () => {
 	errorInfo = document.querySelector(".error-info");
 	addBtn = document.querySelector(".btn-add");
 	ulList = document.querySelector(".todolist ul");
+
+	popup = document.querySelector(".popup");
+	popupInfo = document.querySelector(".popup-info");
+	popupInput = document.querySelector(".popup-input");
+	popupAddBtn = document.querySelector(".accept");
+	popupCloseBtn = document.querySelector(".cancel");
 };
 
 const prepareDOMEvents = () => {
 	addBtn.addEventListener("click", addNewTask);
+	ulList.addEventListener("click", checkClick);
+	popupCloseBtn.addEventListener("click", closePopup);
+	popupAddBtn.addEventListener("click", changeTodoText);
 };
 
 const addNewTask = () => {
@@ -52,6 +68,36 @@ const createToolsArea = (newTask) => {
 	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
 
 	toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
+
+const checkClick = (e) => {
+	if (e.target.matches(".complete")) {
+		e.target.closest("li").classList.toggle("completed");
+		e.target.classList.toggle("completed");
+	} else if (e.target.matches(".edit")) {
+		editTodo(e);
+	} else if (e.target.matches(".delete")) {
+		console.log("ok");
+	}
+};
+
+const editTodo = (e) => {
+	todoToEdit = e.target.closest("li");
+	popupInput.value = todoToEdit.firstChild.textContent;
+	popup.style.display = "flex";
+};
+const closePopup = () => {
+	popup.style.display = "none";
+	popupInfo.textContent = "";
+};
+
+const changeTodoText = () => {
+	if (popupInput.value !== "") {
+		todoToEdit.firstChild.textContent = popupInput.value;
+		closePopup();
+	} else {
+		popupInfo.textContent = "Musisz podać jakąś treść!";
+	}
 };
 
 document.addEventListener("DOMContentLoaded", main);
